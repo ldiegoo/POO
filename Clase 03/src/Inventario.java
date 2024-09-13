f9c87bb4a53cec7371dee8619d65eec2842fc64c
Diego Guzmán <guzman.jdiego@outlook.com>
2024-09-13 16:29:49 -0600

    clase 3

---
+++ Clase 03/src/Inventario.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Inventario {
+    public ArrayList<Producto> listaProductos = new ArrayList<Producto>();
+
+    public void registrarProducto(Producto producto) {
+        this.listaProductos.add(producto);
+    }
+
+    public void eliminarProducto(int idProductoEliminar) {
+        int longitudOriginal = this.listaProductos.size(); // 3
+
+        this.listaProductos.removeIf((producto) -> producto.getId() == idProductoEliminar);
+
+        if (longitudOriginal != this.listaProductos.size()) {
+            System.out.println("Se eliminó el prodcuto con el ID: " + idProductoEliminar);
+        } else {
+            System.out.println("No existe un producto con el ID: " + idProductoEliminar);
+        }
+    }
+
+    public void mostrarProductos() {
+        System.out.println("\n*** PRODUCTOS EN EL SISTEMA ***");
+
+        if (this.listaProductos.size() == 0) {
+            System.out.println("\nNo existen productos en el sistema");
+            return;
+        }
+
+        int iterador = 1;
+        for (Producto producto : this.listaProductos) {
+            System.out.println("\nEste es el producto: " + iterador);
+            System.out.println(producto.mostrarProducto());
+            iterador++;
+        }
+    }
+
+
+}
