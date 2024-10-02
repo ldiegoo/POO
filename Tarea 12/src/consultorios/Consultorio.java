eead386b310b3866629e4a55b90f19b165a590bb
Diego Guzmán <guzman.jdiego@outlook.com>
2024-10-01 21:31:48 -0600

    tarea 12 terminada

---
+++ Tarea 12/src/consultorios/Consultorio.java
@@ -0,0 +1,33 @@
+package consultorios;
+
+
+public class Consultorio {
+    public String id;
+    public int piso;
+    public int numeroConsultorio;
+
+
+    public Consultorio(String id, int piso, int numeroConsultorio) {
+        this.id = id;
+        this.piso = piso;
+        this.numeroConsultorio = numeroConsultorio;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getPiso() {
+        return piso;
+    }
+
+    public int getNumeroConsultorio() {
+        return numeroConsultorio;
+    }
+    public String mostrarConsultorio() {
+        String datos=String.format("Id: %s Piso: %d Num. Consultorio: %d", id, piso, numeroConsultorio);
+        return datos;
+    }
+
+
+}
